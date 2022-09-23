1aa802ac6680b2e2b66ff8207edec94820aba57b
Richard Miao <richardmiao1645@gmail.com>
2022-09-23 13:14:01 -0700

    A function that outputs the tail of an array

--- tail.js
+++ tail.js
@@ -6,3 +6,12 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+const tail = function(array) {
+  let result = array.slice(1);
+  return result;
+};
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
