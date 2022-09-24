7fb0d6f8ea6a9045bfd68547e240c28746dcb9e6
Richard Miao <richardmiao1645@gmail.com>
2022-09-24 16:55:34 -0700

    A function that 'flattens' a array that may contain nested arrays

---
+++ flatten.js
@@ -0,0 +1,15 @@
+const flatten = function(array) {
+  let newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])) {
+      for (let j = 0; j < array[i].length; j++) {
+        newArray.push(array[i][j]);
+      }
+    } else {
+      newArray.push(array[i]);
+    }
+  }
+  return newArray;
+};
+
+console.log(flatten([1, 2, [3, 4], 5, [6]])); // => [1, 2, 3, 4, 5, 6]
