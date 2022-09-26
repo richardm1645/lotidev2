ebac0213642ebaef28803279d7accc3497d3f11c
Richard Miao <richardmiao1645@gmail.com>
2022-09-25 20:28:53 -0700

    A function that returns the key of an object by inputting its value

---
+++ findKeyByValue.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`❤️❤️❤️Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🖕🖕🖕Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(obj, keyValue) {
+  const keys = Object.keys(obj);
+  for (let key of keys) {
+    if (obj[key] === keyValue) {
+      return key;
+    }
+  }
+};
+
+//Test cases
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
