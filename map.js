57789bc189ad31f1aed2a096db3232c9ae939ec9
Richard Miao <richardmiao1645@gmail.com>
2022-09-28 13:56:06 -0700

    A Function that manually works the same way as .map

---
+++ map.js
@@ -0,0 +1,30 @@
+const eqArrays = function(array1, array2) {
+  let longerArray; //The loop needs to be the array with a longer length, otherwise the function doesn't work all the time.
+  if (array1.length > array2.length) {
+    longerArray = array1.length;
+  } else {
+    longerArray = array2.length;
+  }
+  for (let i = 0; i < longerArray; i++) {
+    if (array1[i] !== array2[i]) {
+      console.log(`🖕🖕🖕Assertion Failed: ${array1} !== ${array2}`);
+      return;
+    }
+  }
+  console.log(`❤️❤️❤️Assertion Passed: ${array1} === ${array2}`);
+  return;
+};
+
+//Code here
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+const results1 = map(words, word => word[0]);
+eqArrays(results1, ['g', 'c', 't', 'm', 't']);
